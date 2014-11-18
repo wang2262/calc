7897ed689074e8414d7ae971ec43f018f70ac025
Danyang <wang2262@purdue.edu>
2014-11-18 10:55:40 -0500

    added modulus

--- calc.py
+++ calc.py
@@ -33,6 +33,12 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+# Modulus Division function
+# a -- divided
+# b -- divisor
+def mod(a, b):
+    return a % b
+
 
 # -------------------------------------------------------- #
 
@@ -67,6 +73,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+        elif (op == "%"):
+            print "Remainder: ", mod(a, b)
         else:
             print "Invalid operation..."
 
